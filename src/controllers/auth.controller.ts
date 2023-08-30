58d7b03d8cf9d150f89c29ff5646d04a8b0ba9c3
Alexis Checura <alexischecura@gmail.com>
2023-08-30 11:57:32 -0300

    feat/add user logout endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -217,3 +217,34 @@ export const refreshAccessTokenHandler = async (
     );
   }
 };
+
+// Logout
+
+const invalidateSession = async (res: Response, userId?: string) => {
+  if (userId) await redisClient.del(userId);
+  res.cookie('access_token', '', { maxAge: -1 });
+  res.cookie('refresh_token', '', { maxAge: -1 });
+  res.cookie('logged_in', '', { maxAge: -1 });
+};
+
+// Close user session or sessions
+export const logoutUserHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = res.locals.user?.id;
+
+    const logoutAllSessions = req.query.all === 'true';
+
+    await invalidateSession(res, logoutAllSessions ? undefined : userId);
+  } catch (error) {
+    console.error(error);
+    next(new InternalServerError('Something went wrong when logging out.'));
+  }
+  res.status(200).json({
+    status: 'success',
+    message: 'User successfully logged out',
+  });
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   createUserHandler,
   loginUserHandler,
+  logoutUserHandler,
   refreshAccessTokenHandler,
   verifyEmailHandler,
 } from '../controllers/auth.controller';
@@ -30,7 +31,11 @@ router
   .post('/login', validateBody(loginUserSchema), loginUserHandler)
   .post('/refresh', refreshAccessTokenHandler);
 
+// Authenticated Routes
 router.use(authenticateUser);
 router.get('/me', getCurrentUser);
+
+// Logout user
+router.post('/logout', logoutUserHandler);
 
 export default router;
